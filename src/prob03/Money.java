f941adb192220c30c78d7c2924df38208ef72e3c
bit <bit@DESKTOP-IH79A7R>
2019-01-04 19:32:03 +0900

    과제 완료

--- src/prob03/Money.java
+++ src/prob03/Money.java
@@ -1,8 +1,48 @@
 package prob03;
 
 public class Money {
-
 	private int amount;
-    
-    
+
+	public Money(int amount) {
+		this.amount = amount;
+	}
+
+	public Object add(Money money) {
+		return new Money(amount + money.amount);
+	}
+
+	public Object minus(Money money) {
+		return new Money(amount - money.amount);
+	}
+
+	public Object multiply(Money money) {
+		return new Money(amount * money.amount);
+	}
+
+	public Object devide(Money money) {
+		return new Money(amount / money.amount);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Money other = (Money) obj;
+		if (amount != other.amount)
+			return false;
+		return true;
+	}
+
 }
